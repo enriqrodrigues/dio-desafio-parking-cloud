95a5770fd6815fb24d6c061d33555b2a1cff4c8a
Henrique Rodrigues <mandemsgs@hotmail.com>
2022-08-03 09:23:45 -0300

    First commit

---
+++ src/main/java/com/hnrq/parking/ParkingCloudApplication.java
@@ -0,0 +1,13 @@
+package com.hnrq.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnrq/parking/ParkingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnrq.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
